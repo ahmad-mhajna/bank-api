1bdfcccaf17e12b398ad848c0c0296221000e27c
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-06-16 16:50:34 +0300

    server

--- server/server.js
+++ server/server.js
@@ -2,35 +2,36 @@ const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
 const server = express();
-const pathToUsers = path.join(__dirname, "./data/users.json");
 const port = process.env.PORT || 5000;
+let users = fs.readFileSync(`${__dirname}/data/users.json`).toString();
+console.log(users);
 server.use(express.json());
-server.get("/numbers", (req, res) => {
+server.get("/users", (req, res) => {
   res.send(users);
 });
 
-server.post("/numbers", (req, res) => {
-  if (numbers.includes(...req.body)) {
+server.post("/users", (req, res) => {
+  if (users.includes(...req.body)) {
     return res.status(400).send("Number already Exists!");
   }
-  numbers.push(...req.body);
-  res.send(numbers);
+  users.push(...req.body);
+  res.send(users);
 });
-server.delete("/numbers/:number", (req, res) => {
+server.delete("/users/:number", (req, res) => {
   const number = Number(req.params.number);
-  if (!numbers.includes(number)) {
+  if (!users.includes(number)) {
     return res.status(400).send("this number does not exist");
   }
-  numbers = numbers.filter((num) => num !== number);
-  res.send(numbers);
+  users = users.filter((num) => num !== number);
+  res.send(users);
 });
-server.put("/numbers/:number", (req, res) => {
+server.put("/users/:number", (req, res) => {
   const number = Number(req.params.number);
-  if (!numbers.includes(number)) {
+  if (!users.includes(number)) {
     return res.status(400).send("this number does not exist");
   }
-  numbers.splice(numbers.indexOf(number), 1, ...req.body);
-  res.send(numbers);
+  users.splice(users.indexOf(number), 1, ...req.body);
+  res.send(users);
 });
 
 server.listen(port, () => {
