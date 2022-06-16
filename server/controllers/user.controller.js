82be71c737772f2aa45bbbc4d1d8230f58acc388
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-06-16 23:07:26 +0300

    fixed little bugs and done hero need more testing

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,33 +2,59 @@ const fs = require("fs");
 const path = require("path");
 const pathToData = path.resolve(__dirname, "../data/users.json");
 let users = JSON.parse(fs.readFileSync(pathToData).toString());
+
 const getUsers = (req, res) => {
   res.send(users);
 };
+
 const getUser = (req, res) => {
   if (!users.find((user) => user.id === +req.params.id)) {
     return res.status(400).send("this user dose not exist");
   }
   res.send(users.find((user) => user.id === +req.params.id));
 };
-const getUserByamount = (req, res) => {
-  if (users.find((user) => user.cash !== req.params.amount)) {
+
+const getUsersByAmount = (req, res) => {
+  if (!users.find((user) => user.cash === +req.params.amount)) {
     return res.status(400).send("this user dose not exist");
   }
-  res.send(users.find((user) => user.cash === req.params.amount));
+  const usersfillterd = users.filter(
+    (user) => user.cash === +req.params.amount
+  );
+  res.send(usersfillterd);
 };
+
+const getUsersByIsActive = (req, res) => {
+  let isActive =
+    req.params.isActive === "true"
+      ? true
+      : req.params.isActive === "false"
+      ? false
+      : "";
+
+  if (isActive === "") {
+    return res.status(400).send("invalid isActive param");
+  }
+  if (!users.find((user) => user.isActive === isActive)) {
+    return res.status(400).send("this user dose not exist");
+  }
+  const usersfillterd = users.filter((user) => user.isActive === isActive);
+  res.send(usersfillterd);
+};
+
 const postUser = (req, res) => {
   if (users.find((user) => user.id === req.body.id)) {
     return res.status(400).send("user already Exists!");
   }
   const user = {
     id: req.body.id,
-    cash: req.body.cash ?? 0,
-    credit: req.body.credit ?? 0,
+    cash: +req.body.cash ?? 0,
+    credit: +req.body.credit ?? 0,
   };
   users.push(user);
   res.send(users);
 };
+
 const deleteUser = (req, res) => {
   const id = +req.params.id;
   if (!users.find((user) => user.id === id)) {
@@ -41,6 +67,7 @@ const deleteUser = (req, res) => {
 const putUser = (req, res) => {
   const type = req.params.type;
   let { amount, id } = req.body;
+  if (!amount) return res.status(400).send("Amount is undefined");
   let user = users.find((user) => user.id === id);
   if (amount < 0) {
     return res.status(400).send("amount cant be negative");
@@ -74,12 +101,14 @@ const putUser = (req, res) => {
   }
   res.send(users);
 };
+console.log(users);
 
 module.exports = {
-  getUserByamount,
+  getUsersByAmount,
   getUsers,
   getUser,
   postUser,
   deleteUser,
   putUser,
+  getUsersByIsActive,
 };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -6,10 +6,13 @@ const {
   postUser,
   deleteUser,
   putUser,
+  getUsersByAmount,
+  getUsersByIsActive,
 } = require("../controllers/user.controller");
 UserRouter.get("/users", getUsers);
 UserRouter.get("/users/:id", getUser);
-UserRouter.get("/users/:amount", getUser);
+UserRouter.get("/users/amount/:amount", getUsersByAmount);
+UserRouter.get("/users/isActive/:isActive", getUsersByIsActive);
 UserRouter.post("/users", postUser);
 UserRouter.delete("/users/:id", deleteUser);
 UserRouter.put("/users/:type", putUser);
